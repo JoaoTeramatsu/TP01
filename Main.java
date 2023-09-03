54f979ae3afd39844b0b4b7b3bc84974e9cd9351
João Teramatsu <jv.teramatsu@bol.com.br>
2023-09-03 18:23:55 -0300

    ~ CRUD init

--- Main.java
+++ Main.java
@@ -6,25 +6,22 @@ import java.util.Scanner;
 
 public class Main {
    public static void main(String args[]) throws IOException {
+      CRUD crud = new CRUD("pokemonDB");
+      String basefile = "pokemonSample.csv";
       try {
-         CRUD crud = new CRUD("pokemonDB");
-         String basefile = "./pokemonSample.csv";
-         try {
-            FileInputStream fs = new FileInputStream(basefile);
-            BufferedReader br = new BufferedReader(new InputStreamReader(fs));
-            String csvLine;
-
-            while ((csvLine = br.readLine()) != null) {
-               Pokemon pokemon = new Pokemon();
-               pokemon.parseCSV(csvLine);
-               crud.create(pokemon);
-            }
-            br.close();
-         } catch (Exception e) {
-            e.printStackTrace();
+         FileInputStream fs = new FileInputStream(basefile);
+         BufferedReader br = new BufferedReader(new InputStreamReader(fs));
+         String csvLine = br.readLine();
+         while ((csvLine = br.readLine()) != null) {
+            Pokemon pokemon = new Pokemon();
+            System.out.println(csvLine);
+            pokemon.parseCSV(csvLine);
+            crud.create(pokemon);
          }
+
+         br.close();
       } catch (Exception e) {
-         System.out.println("Erro: " + e);
+         e.printStackTrace();
       }
       Scanner fetch = new Scanner(System.in);
       fetch.close();
