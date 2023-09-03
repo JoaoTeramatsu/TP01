f0dd00b65dc0b1dade85f400a35223bd0b1c0588
João Teramatsu <jv.teramatsu@bol.com.br>
2023-09-03 18:23:46 -0300

    ~ CRUD init

--- CRUD.java
+++ CRUD.java
@@ -4,8 +4,22 @@ import java.io.IOException;
 public class CRUD {
    private RandomAccessFile file;
 
-   CRUD(String filename) throws IOException{
+   CRUD(String filename) throws IOException {
       this.file = new RandomAccessFile(filename, "rw");
+   }
+
+   public void create(RandomAccessFile file, Pokemon pokemon) throws IOException {
+      file.seek(0);
+      file.writeInt(pokemon.getIndex());
+      file.seek(file.length());
+
+      byte[] byteArr = pokemon.toByteArray();
+      file.writeInt(pokemon.toByteArray().length);
+      file.write(byteArr);
+   }
+
+   public void create(Pokemon pokemon) throws IOException {
+      create(this.file, pokemon);
    }
 
 }
--- Main.java
+++ Main.java
@@ -1,20 +1,32 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
-   public static void main(String args[]){
-      try{
-         String basefile = "./pokemon.csv";
-         FileInputStream fs = new FileInputStream(basefile);
-         BufferedReader br = new BufferedReader(new InputStreamReader(fs));
+   public static void main(String args[]) throws IOException {
+      try {
+         CRUD crud = new CRUD("pokemonDB");
+         String basefile = "./pokemonSample.csv";
+         try {
+            FileInputStream fs = new FileInputStream(basefile);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fs));
+            String csvLine;
 
-         String csvLine;
-         while((csvLine = br.readLine()) != null){
-            Pokemon pokemon = new Pokemon();
-            pokemon.parseCSV(csvLine); 
+            while ((csvLine = br.readLine()) != null) {
+               Pokemon pokemon = new Pokemon();
+               pokemon.parseCSV(csvLine);
+               crud.create(pokemon);
+            }
+            br.close();
+         } catch (Exception e) {
+            e.printStackTrace();
          }
-
-      }catch(Exception e){System.out.println("Erro: " + e);}
+      } catch (Exception e) {
+         System.out.println("Erro: " + e);
+      }
+      Scanner fetch = new Scanner(System.in);
+      fetch.close();
    }
 }
