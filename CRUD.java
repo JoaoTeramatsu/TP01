57aa0e81542e3c656c8236786c00181c885c416c
Joao Teramatsu <joaovictor.teramatsu@gmail.com>
2023-09-04 10:20:26 -0300

    + READ & binToPokemon

--- CRUD.java
+++ CRUD.java
@@ -11,6 +11,7 @@ public class CRUD {
    public void create(RandomAccessFile file, Pokemon pokemon) throws IOException {
       file.seek(0);
       file.writeInt(pokemon.getIndex());
+      System.out.println(file.length());
       file.seek(file.length());
 
       byte[] byteArr = pokemon.toByteArray();
@@ -21,5 +22,36 @@ public class CRUD {
    public void create(Pokemon pokemon) throws IOException {
       create(this.file, pokemon);
    }
+   //Fazer Método READ
+   public void read(int pokemonIndex) throws IOException{
+      long pos;
+      int bytes, id;
+      boolean isAlive;
 
+      Pokemon pokemon = null;
+
+      file.seek(0);
+      file.readInt(); //Lê o index do registro x
+      try{
+         while(file.getFilePointer() < file.length()){
+            pos = file.getFilePointer();
+            bytes = file.readInt();
+            isAlive = file.readBoolean();
+            if(file.readInt() == pokemonIndex && isAlive){
+               try{
+                  //convertTo
+                  break;
+               }catch(Exception e){e.printStackTrace();}
+            }else{
+               //entender esse -5
+               file.skipBytes(pokemonIndex - 5);
+            }
+         }
+      }catch(IOException e){e.printStackTrace();}
+   }
+   
+   public Pokemon binToPokemon(RandomAccessFile file, long pos){
+      Pokemon pokemon = new Pokemon();
+      return pokemon;
+   }
 }
